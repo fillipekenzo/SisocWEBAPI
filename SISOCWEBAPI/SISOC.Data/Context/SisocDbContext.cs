ca6dec4a3909b20b9c9028a2d5a4abb4abab0867
fillipekenzo <fillipekenzo123@gmail.com>
2022-10-24 23:42:09 -0400

    seeds

--- SISOCWEBAPI/SISOC.Data/Context/SisocDbContext.cs
+++ SISOCWEBAPI/SISOC.Data/Context/SisocDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore;
 using SISOC.Business.Models;
+using SISOC.Util.Criptografia;
 
 namespace SISOC.Data.Context
 {
@@ -35,6 +36,12 @@ namespace SISOC.Data.Context
 			foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
 
 			base.OnModelCreating(modelBuilder);
+			SeedSetor(modelBuilder);
+			SeedTipoUsuario(modelBuilder);
+			SeedTipoOcorrencia(modelBuilder);
+			SeedMenuSubmenu(modelBuilder);
+			SeedPermissao(modelBuilder);
+			SeedUsuario(modelBuilder);
 		}
 
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -59,6 +66,85 @@ namespace SISOC.Data.Context
 			}
 
 			return base.SaveChangesAsync(cancellationToken);
+		}
+
+		private void SeedSetor(ModelBuilder builder)
+		{
+			builder.Entity<Setor>().HasData(
+				new Setor() { SetorID = 1, Nome = "Conselho Superior", Sigla = "COSUP", DataHoraCadastro = DateTime.Now },
+				new Setor() { SetorID = 2, Nome = "Reitoria", Sigla = "RTRIA", DataHoraCadastro = DateTime.Now },
+				new Setor() { SetorID = 3, Nome = "Diretoria de Ensino", Sigla = "DIREN", DataHoraCadastro = DateTime.Now }
+				);
+		}
+
+		private void SeedTipoUsuario(ModelBuilder builder)
+		{
+			builder.Entity<TipoUsuario>().HasData(
+				new TipoUsuario() { TipoUsuarioID = 1, Nome = "ADMIN", Descricao = "Usuário Admnistrador", DataHoraCadastro = DateTime.Now },
+				new TipoUsuario() { TipoUsuarioID = 2, Nome = "ESTUDANTE", Descricao = "Usuário Estudante", DataHoraCadastro = DateTime.Now },
+				new TipoUsuario() { TipoUsuarioID = 3, Nome = "DOCENTE", Descricao = "Usuário Docente", DataHoraCadastro = DateTime.Now }
+				);
+		}
+
+		private void SeedUsuario(ModelBuilder builder)
+		{
+			Usuario usuario = new Usuario()
+			{
+				UsuarioID = 1,
+				Nome = "ADMIN",
+				Email = "admin@email.com",
+				Senha = Cripto.HashPassword("1234"),
+				SetorID = 1,
+				RA_SIAPE = "123",
+				TipoUsuarioID = 1,
+			};
+			builder.Entity<Usuario>().HasData(usuario);
+		}
+
+		private void SeedMenuSubmenu(ModelBuilder builder)
+		{
+			builder.Entity<Menu>().HasData(
+				new Menu() { MenuID = 1, Nome = "Menu", NavegarURL = "menu", Ativo = true, Ordem = 1, DataHoraCadastro = DateTime.Now },
+				new Menu() { MenuID = 2, Nome = "Setor", NavegarURL = "setor", Ativo = true, Ordem = 2, DataHoraCadastro = DateTime.Now },
+				new Menu() { MenuID = 3, Nome = "Permissão", NavegarURL = "permissao", Ativo = true, Ordem = 3, DataHoraCadastro = DateTime.Now },
+				new Menu() { MenuID = 4, Nome = "Tipo Ocorrência", NavegarURL = "tipo-ocorrencia", Ativo = true, Ordem = 4, DataHoraCadastro = DateTime.Now },
+				new Menu() { MenuID = 5, Nome = "Tipo Usuário", NavegarURL = "tipo-usuario", Ativo = true, Ordem = 5, DataHoraCadastro = DateTime.Now },
+				new Menu() { MenuID = 6, Nome = "Usuário", NavegarURL = "usuario", Ativo = true, Ordem = 6, DataHoraCadastro = DateTime.Now },
+				new Menu() { MenuID = 7, Nome = "Ocorrência", NavegarURL = "ocorrencia", Ativo = true, Ordem = 7, DataHoraCadastro = DateTime.Now }
+				);
+
+			builder.Entity<Submenu>().HasData(
+				new Submenu() { SubmenuID = 1, MenuID = 7, Nome = "Cadastrar", NavegarURL = "cadastrar", Ativo = true, Ordem = 1, DataHoraCadastro = DateTime.Now },
+				new Submenu() { SubmenuID = 2, MenuID = 7, Nome = "Consultar", NavegarURL = "consultar", Ativo = true, Ordem = 2, DataHoraCadastro = DateTime.Now }
+				);
+		}
+
+		private void SeedPermissao(ModelBuilder builder)
+		{
+			builder.Entity<Permissao>().HasData(
+				new Permissao() { PermissaoID = 1, MenuID = 1, SubmenuID = 1, Cadastrar = true, Consultar = true, Editar = true, Excluir = true, TipoUsuarioID = 1, DataHoraCadastro = DateTime.Now },
+				new Permissao() { PermissaoID = 2, MenuID = 2, SubmenuID = 1, Cadastrar = true, Consultar = true, Editar = true, Excluir = true, TipoUsuarioID = 1, DataHoraCadastro = DateTime.Now },
+				new Permissao() { PermissaoID = 3, MenuID = 3, SubmenuID = 1, Cadastrar = true, Consultar = true, Editar = true, Excluir = true, TipoUsuarioID = 1, DataHoraCadastro = DateTime.Now },
+				new Permissao() { PermissaoID = 4, MenuID = 4, SubmenuID = 1, Cadastrar = true, Consultar = true, Editar = true, Excluir = true, TipoUsuarioID = 1, DataHoraCadastro = DateTime.Now },
+				new Permissao() { PermissaoID = 5, MenuID = 5, SubmenuID = 1, Cadastrar = true, Consultar = true, Editar = true, Excluir = true, TipoUsuarioID = 1, DataHoraCadastro = DateTime.Now },
+				new Permissao() { PermissaoID = 6, MenuID = 6, SubmenuID = 1, Cadastrar = true, Consultar = true, Editar = true, Excluir = true, TipoUsuarioID = 1, DataHoraCadastro = DateTime.Now },
+				new Permissao() { PermissaoID = 7, MenuID = 7, SubmenuID = 1, Cadastrar = true, Consultar = true, Editar = true, Excluir = true, TipoUsuarioID = 1, DataHoraCadastro = DateTime.Now },
+
+				new Permissao() { PermissaoID = 8, MenuID = 7, SubmenuID = 1, Cadastrar = true, Consultar = true, Editar = true, Excluir = false, TipoUsuarioID = 2, DataHoraCadastro = DateTime.Now },
+
+				new Permissao() { PermissaoID = 9, MenuID = 7, SubmenuID = 1, Cadastrar = true, Consultar = true, Editar = true, Excluir = false, TipoUsuarioID = 3, DataHoraCadastro = DateTime.Now }
+
+				);
+		}
+
+		private void SeedTipoOcorrencia(ModelBuilder builder)
+		{
+			builder.Entity<TipoOcorrencia>().HasData(
+				new TipoOcorrencia() { TipoOcorrenciaID = 1, Nome = "Aluno", Descricao = "Incidentes com Alunos", DataHoraCadastro = DateTime.Now },
+				new TipoOcorrencia() { TipoOcorrenciaID = 2, Nome = "Campus", Descricao = "Incidentes com o Câmpus", DataHoraCadastro = DateTime.Now },
+				new TipoOcorrencia() { TipoOcorrenciaID = 3, Nome = "Sala de Aula", Descricao = "Incidentes dentro da Sala de Aula", DataHoraCadastro = DateTime.Now },
+				new TipoOcorrencia() { TipoOcorrenciaID = 4, Nome = "Docente", Descricao = "Incidentes com Docente", DataHoraCadastro = DateTime.Now }
+				);
 		}
 	}
 }
---
+++ SISOCWEBAPI/SISOC.Data/Migrations/20221025031940_seed.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SISOC.Data.Migrations
+{
+    public partial class seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Menu",
+                columns: new[] { "MenuID", "Ativo", "DataHoraAlteracao", "DataHoraCadastro", "NavegarURL", "Nome", "Ordem" },
+                values: new object[,]
+                {
+                    { 1, true, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(802), "menu", "Menu", 1 },
+                    { 2, true, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(804), "setor", "Setor", 2 },
+                    { 3, true, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(805), "permissao", "Permissão", 3 },
+                    { 4, true, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(807), "tipo-ocorrencia", "Tipo Ocorrência", 4 },
+                    { 5, true, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(808), "tipo-usuario", "Tipo Usuário", 5 },
+                    { 6, true, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(809), "usuario", "Usuário", 6 },
+                    { 7, true, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(838), "ocorrencia", "Ocorrência", 7 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Setor",
+                columns: new[] { "SetorID", "DataHoraAlteracao", "DataHoraCadastro", "Nome", "Sigla" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(674), "Conselho Superior", "COSUP" },
+                    { 2, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(683), "Reitoria", "RTRIA" },
+                    { 3, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(684), "Diretoria de Ensino", "DIREN" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "TipoOcorrencia",
+                columns: new[] { "TipoOcorrenciaID", "DataHoraAlteracao", "DataHoraCadastro", "Descricao", "Nome" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(782), "Incidentes com Alunos", "Aluno" },
+                    { 2, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(783), "Incidentes com o Câmpus", "Campus" },
+                    { 3, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(784), "Incidentes dentro da Sala de Aula", "Sala de Aula" },
+                    { 4, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(785), "Incidentes com Docente", "Docente" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "TipoUsuario",
+                columns: new[] { "TipoUsuarioID", "DataHoraAlteracao", "DataHoraCadastro", "Descricao", "Nome" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(767), "Usuário Admnistrador", "ADMIN" },
+                    { 2, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(768), "Usuário Estudante", "ESTUDANTE" },
+                    { 3, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(769), "Usuário Docente", "DOCENTE" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Submenu",
+                columns: new[] { "SubmenuID", "Ativo", "DataHoraAlteracao", "DataHoraCadastro", "MenuID", "NavegarURL", "Nome", "Ordem" },
+                values: new object[] { 1, true, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(855), 7, "cadastrar", "Cadastrar", 1 });
+
+            migrationBuilder.InsertData(
+                table: "Submenu",
+                columns: new[] { "SubmenuID", "Ativo", "DataHoraAlteracao", "DataHoraCadastro", "MenuID", "NavegarURL", "Nome", "Ordem" },
+                values: new object[] { 2, true, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(856), 7, "consultar", "Consultar", 2 });
+
+            migrationBuilder.InsertData(
+                table: "Usuario",
+                columns: new[] { "UsuarioID", "DataHoraAlteracao", "DataHoraCadastro", "Email", "Nome", "RA_SIAPE", "Senha", "SetorID", "TipoUsuarioID" },
+                values: new object[] { 1, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "admin@email.com", "ADMIN", "123", "AJXuxI6QmxNLR5fHeHAkTTsJbpHQJlxwOJNCmYScfUPJEru1ezmHyrIOGkFMcZZ1Ng==", 1, 1 });
+
+            migrationBuilder.InsertData(
+                table: "Permissao",
+                columns: new[] { "PermissaoID", "Cadastrar", "Consultar", "DataHoraAlteracao", "DataHoraCadastro", "Editar", "Excluir", "MenuID", "SubmenuID", "TipoUsuarioID" },
+                values: new object[,]
+                {
+                    { 1, true, true, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(871), true, true, 1, 1, 1 },
+                    { 2, true, true, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(872), true, true, 2, 1, 1 },
+                    { 3, true, true, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(873), true, true, 3, 1, 1 },
+                    { 4, true, true, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(875), true, true, 4, 1, 1 },
+                    { 5, true, true, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(876), true, true, 5, 1, 1 },
+                    { 6, true, true, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(877), true, true, 6, 1, 1 },
+                    { 7, true, true, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(878), true, true, 7, 1, 1 },
+                    { 8, true, true, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(879), true, false, 7, 1, 2 },
+                    { 9, true, true, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2022, 10, 24, 23, 19, 40, 262, DateTimeKind.Local).AddTicks(880), true, false, 7, 1, 3 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Permissao",
+                keyColumn: "PermissaoID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Permissao",
+                keyColumn: "PermissaoID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Permissao",
+                keyColumn: "PermissaoID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Permissao",
+                keyColumn: "PermissaoID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Permissao",
+                keyColumn: "PermissaoID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Permissao",
+                keyColumn: "PermissaoID",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Permissao",
+                keyColumn: "PermissaoID",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Permissao",
+                keyColumn: "PermissaoID",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Permissao",
+                keyColumn: "PermissaoID",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Setor",
+                keyColumn: "SetorID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Setor",
+                keyColumn: "SetorID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Submenu",
+                keyColumn: "SubmenuID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "TipoOcorrencia",
+                keyColumn: "TipoOcorrenciaID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "TipoOcorrencia",
+                keyColumn: "TipoOcorrenciaID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "TipoOcorrencia",
+                keyColumn: "TipoOcorrenciaID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "TipoOcorrencia",
+                keyColumn: "TipoOcorrenciaID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Usuario",
+                keyColumn: "UsuarioID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Menu",
+                keyColumn: "MenuID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Menu",
+                keyColumn: "MenuID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Menu",
+                keyColumn: "MenuID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Menu",
+                keyColumn: "MenuID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Menu",
+                keyColumn: "MenuID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Menu",
+                keyColumn: "MenuID",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Setor",
+                keyColumn: "SetorID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Submenu",
+                keyColumn: "SubmenuID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "TipoUsuario",
+                keyColumn: "TipoUsuarioID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "TipoUsuario",
+                keyColumn: "TipoUsuarioID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "TipoUsuario",
+                keyColumn: "TipoUsuarioID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Menu",
+                keyColumn: "MenuID",
+                keyValue: 7);
+        }
+    }
+}
